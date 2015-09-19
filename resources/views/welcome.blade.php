ae42ab5acef6e307205dbd113459198be0e1e152
Josh Rogan <jjjr1122@gmail.com>
2015-09-19 01:15:11 -0400

    Update button styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,8 +40,22 @@
 
     	<aside class='sidebar'>
     		<h2> Side Bar Content </h2>
-    		<button> Request Appointment </button>
-    		<button> Patient Reviews </button>
+    		<div class="button-wrapper">
+	    		<button> <span class='icon'><i class="fa fa-calendar-plus-o"></i></span> Request Appointment</button>
+	    		<button> <span class='icon'><i class="fa fa-star"></i></span> Patient Reviews </button>
+	    	</div>
+
+    		<h3 class='youtube'> <i class="fa fa-youtube-square"></i> iOC Scanner </h3>
+    		<div class="embed-responsive embed-responsive-16by9">
+			  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/iZcwXGi1CTs"></iframe>
+			</div>
+			<div class="clearfix">&nbsp;</div>
+			<div class="button-wrapper">
+			<button> <span class='icon'><i class="fa fa-file-text"></i></span> Dentist/Physician Referral </button>
+    		<button> <span class='icon'><i class="fa fa-newspaper-o"></i></span> What's Happening!</button>
+    		<button> <span class='icon'><i class="fa fa-book"></i></span> Dental Library </button>
+    		<button> <span class='icon'><i class="fa fa-files-o"></i></span> eBrochure </button>
+    		</div>
     	</aside>
     </div>
 </div>
