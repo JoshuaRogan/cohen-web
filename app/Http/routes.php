1272143882043f9a67d46204835e6a06bcad1f2d
Josh Rogan <jjjr1122@gmail.com>
2015-09-19 22:35:46 -0400

    Create routes to handle everything

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,40 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
+/**
+ * Three Level catch all for static website generator. Try to find an appropiate template.
+ * Protect certain directories that shouldn't be accessed and add additional directoires to
+ * div into.  
+ * @return Response Return a valid response for any request 
+ */
+Route::get('{slug?}/{slug2?}/{slug3?}', function (Request $request) {
+	$blocked_directories = array("analytics", "errors", "mixins", "navigation", "skeleton");
+	$check_directories = array("locations", "services", "pages");
+
+    //Try to find a blade template
+    $potential_view = strtolower(str_replace("/", ".", $request->path()));
+    $root_dir = array_values(explode(".", $potential_view))[0];
+
+    //Don't allow certain directories to be rendered
+    if(in_array($root_dir, $blocked_directories) || strlen($root_dir) == 0){
+    	return view('welcome'); 
+    }
+    
+    //Try to find the view
+    if(View::exists($potential_view)){
+    	return view($potential_view);
+    }
+    else{
+    	//Check all check directories for the template 
+    	foreach ($check_directories as $key) {
+    		if(View::exists("$key." . $potential_view)){
+    			return view("$key." . $potential_view);
+    		}
+    	}
+    	
+    }
+
+    abort(404); //If nothing is found report 404
 });
