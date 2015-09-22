0bc3aa007560e4308351c3d7316808a6df003ad8
Josh Rogan <jjjr1122@gmail.com>
2015-09-22 03:52:03 -0400

    Updates

--- resources/views/skeleton/footer.blade.php
+++ resources/views/skeleton/footer.blade.php
@@ -1,42 +1,73 @@
 @section('footer')
 <footer>
-	<div class='footer-container'>
-		<div class="sitemap">
+	<div class='footer-container row'>
+		
+
+		<div class="services">
+			<h2><a href='#'>For Patients</a></h2>
 			<ul>
-				<li>Sitemap</li>
-				<li><a href='#'>Home</a></li>
-				<li><a href='#'>About Us</a></li>
-				<li><a href='#'>Meet Your Team</a></li>
-				<li><a href='#'>For Patients</a></li>
-				<li><a href='#'>Testimonials</a></li>
-				<li><a href='#'>Contact Us</a></li>
-				<li><a href='#'>Privacy Policy</a></li>
+				<li><a href='/for-patients/faqs'>FAQs </a></li>
+				<li><a href='/for-patients/your-first-visit'>Early Intervention </a></li>
+				<li><a href='/before-and-after-gallery'>Before and After Gallery</a></li>
+			</ul>
+		</div>
+
+		<div class="about">
+			<h2> <a href='#'>About Us</a></h2>
+			<ul> 
+				<li><a href='/contact'>Locations</a></li>
+				<li><a href='/meet-your-team'>Meet Your Team</a></li>
 			</ul>
 		</div>
 
 		<div class="services">
+			<h2><a href='#'>Services</a></h2>
 			<ul>
-				<li>Services</li>
-				<li><a href='#'>Traditional Braces </a></li>
-				<li><a href='#'>Early Intervention </a></li>
-				<li><a href='#'>Invisalign Teen </a></li>
-				<li><a href='#'>Invisalign </a></li>
-				<li><a href='#'>Adult Orthodontics </a></li>
+				<li> <a href='/our-services/invisalign'>Invisalign</a></li>
+				<li> <a href='/our-services/adult-orthodontics'>Adult Orthodontics</a></li>
+	            <li> <a href='/our-services/early-intervention'>Early Intervention</a></li>
+	            <li> <a href='/our-services/invisalign-teen'>Invisalign Teen</a></li>
+	            <li> <a href='/our-services/traditional-braces'>Traditional Braces</a></li>
 			</ul>
 		</div>
+
+
+		<div class="sitemap">
+			<h2><a href='#'>Sitemap</a></h2>
+			<ul>
+				<li><a href='/'>Home</a></li>
+				<li><a href='about-us'>About Us</a></li>
+				<li><a href='/meet-your-time'>Meet Your Team</a></li>
+				<li><a href='/for-patiens'>For Patients</a></li>
+				<li><a href='/patient-reviews'>Testimonials</a></li>
+				<li><a href='/contact'>Contact Us</a></li>
+				<li><a href='#'>Privacy Policy</a></li>
+
+				<li><a href="/about-us">About Us </a></li>
+                    <li><a href="/before-and-after-gallery">Smile Gallery </a></li>
+                    <li><a href="/contact">Locations </a></li>
+			</ul>
+		</div>
+
+		<div class="clearfix"></div>
+
+		<div class="qoute">
+			<p>Mitchell S. Cohen, DDS, MSD offers comprehensive teeth-straightening methods for people of allages including traditional braces, ceramic (clear) braces and clear aligners. Invisalign® virtually invisible aligners give teenagers and adults the opportunity of a beautiful smile without using bulky metal braces. Dr. Mitchell Cohen is always willing to discuss other options including the Damon System, Clear Correct and sub-lingual aligners. With three convenient locations in Northeastern Pennsylvania Dr. Cohen provides orthodontic treatment to the communities in Lackawanna and Luzerne counties. </p>
+		</div>
+
+
 
 		<div class="locations">
 			<ul>
-				<li>Locations</li>
-				<li><a href='#'>Scranton Route 6 Fashion Mall (570) 346-2132 </a></li>
-				<li><a href='#'>Pittston 100 Kennedy Blvd. (570) 654-7117  </a></li>
-				<li><a href='#'>Wilkes-Barre 900 Schechter Dr. (570) 822-7700 </a></li>
+				<li><a href='/contact#scranton'>Scranton Route 6 Fashion Mall (570) 346-2132 </a></li>
+				<li><a href='/contact#pittston'>Pittston 100 Kennedy Blvd. (570) 654-7117  </a></li>
+				<li><a href='/contact#wilkes'>Wilkes-Barre 900 Schechter Dr. (570) 822-7700 </a></li>
 			</ul>
 		</div>
 
-		<div class="quote">
-			<p>Mitchell S. Cohen, DDS, MSD offers comprehensive teeth-straightening methods for people of allages including traditional braces, ceramic (clear) braces and clear aligners. Invisalign® virtually invisible aligners give teenagers and adults the opportunity of a beautiful smile without using bulky metal braces. Dr. Mitchell Cohen is always willing to discuss other options including the Damon System, Clear Correct and sub-lingual aligners. With three convenient locations in Northeastern Pennsylvania Dr. Cohen provides orthodontic treatment to the communities in Lackawanna and Luzerne counties.</p>
-		</div>
+		
+
+		<div class="clearfix"></div>
 
 		<div class="copyright">
 			Copyright ©  {{date("Y")}} Mitchell Cohen Orthodontics. <strong>All rights reserved</strong>.
